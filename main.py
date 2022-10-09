753060481c3a2887e87039e203d627f1923078a2
My name <me@example.com>
2022-10-09 16:36:59 +0300

    toplamalar ekledim

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-print("hello world")
+from ege import summ
 
+print("hello world")
 
 a = 5
 b = 10
+
 c = 20
-d=a+b
-e = c+a
-f = a+b+c
-g = d+f
+d = a + b
+
+e = c + a
+
+f = a + b + c
+g = d + f
+z = 52 * 8
+x = summ(a, b)
 
 print(g)
